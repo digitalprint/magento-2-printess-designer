842da7a74ba21064cf721226aad444030300840d
Stephan Weikert <weikert@digital-print.net>
2022-01-28 15:12:10 +0100

    Add printess values to order api response

---
+++ Plugin/Orders/CustomProductAttributes.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Digitalprint\PrintessDesigner\Plugin\Orders;
+
+use Magento\Catalog\Model\ProductFactory;
+use Magento\Sales\Api\Data\OrderItemInterface;
+use Magento\Sales\Api\OrderItemRepositoryInterface;
+use Magento\Sales\Api\Data\OrderItemExtensionFactory;
+use Magento\Sales\Api\Data\OrderItemSearchResultInterface;
+
+class CustomProductAttributes
+{
+
+    /**
+     * @var OrderItemExtensionFactory
+     */
+    protected $orderItemExtensionFactory;
+
+    /**
+     * @var ProductFactory
+     */
+    protected $productFactory;
+
+    /**
+     * @param OrderItemExtensionFactory $orderItemExtensionFactory
+     * @param ProductFactory $productFactory
+     */
+    public function __construct(
+        OrderItemExtensionFactory $orderItemExtensionFactory,
+        ProductFactory $productFactory
+    ) {
+
+        $this->orderItemExtensionFactory = $orderItemExtensionFactory;
+        $this->productFactory = $productFactory;
+    }
+
+    public function afterGet(OrderItemRepositoryInterface $subject, OrderItemInterface $orderItem)
+    {
+        $extensionAttributes = $orderItem->getExtensionAttributes();
+        $extensionAttributes = $extensionAttributes ?: $this->orderItemExtensionFactory->create();
+
+        $options = $orderItem->getProductOptions();
+
+        if (isset($options['options']['printess_save_token'])) {
+            $extensionAttributes->setPrintessSaveToken($options['options']['printess_save_token']['value']);
+        }
+
+        if (isset($options['options']['printess_thumbnail_url'])) {
+            $extensionAttributes->setPrintessThumbnailUrl($options['options']['printess_thumbnail_url']['value']);
+        }
+
+        $orderItem->setExtensionAttributes($extensionAttributes);
+
+        return $orderItem;
+    }
+
+
+    public function afterGetList(OrderItemRepositoryInterface $subject, OrderItemSearchResultInterface $result)
+    {
+
+        $orderItems = $result->getItems();
+
+        foreach($orderItems as $orderItem) {
+
+            $extensionAttributes = $orderItem->getExtensionAttributes();
+            $extensionAttributes = $extensionAttributes ?: $this->orderItemExtensionFactory->create();
+
+            $options = $orderItem->getProductOptions();
+
+            if (isset($options['options']['printess_save_token'])) {
+                $extensionAttributes->setPrintessSaveToken($options['options']['printess_save_token']['value']);
+            }
+
+            if (isset($options['options']['printess_thumbnail_url'])) {
+                $extensionAttributes->setPrintessThumbnailUrl($options['options']['printess_thumbnail_url']['value']);
+            }
+
+            $orderItem->setExtensionAttributes($extensionAttributes);
+
+        }
+
+        return $result;
+    }
+}
