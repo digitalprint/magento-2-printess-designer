c4918a358bec16d3a7e286331ed246863c968bdb
Stephan Weikert <weikert@digital-print.net>
2022-02-22 10:45:43 +0100

    Update bootstrap 5.0.2

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,6 +1,6 @@
 var config = {
     paths: {
-        'bootstrap': ['https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min'],
+        'bootstrap': ['https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min'],
         'webcomponents-loader': ['https://cdn.jsdelivr.net/npm/@webcomponents/webcomponentsjs@2.6.0/webcomponents-loader'],
         'polyfill-fetch': ['https://polyfill.io/v3/polyfill.min.js?features=fetch']
     }
