35f761638a90a2bc6da31fb449fd5e08780bea8e
Stephan Weikert <weikert@digital-print.net>
2023-09-13 09:51:49 +0200

    Bugfix modify only when custom options are available

--- Plugin/Adminhtml/Catalog/CustomOptions.php
+++ Plugin/Adminhtml/Catalog/CustomOptions.php
@@ -7,45 +7,48 @@ class CustomOptions
 
     /**
      * @param \Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\CustomOptions $subject
-     * @param array $meta
+     * @param array $result
      * @return array
      */
-    public function afterModifyMeta(\Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\CustomOptions $subject, array $meta): array
+    public function afterModifyMeta(\Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\CustomOptions $subject, array $result): array
     {
 
-       return array_merge_recursive($meta, [
-            \Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\CustomOptions::GROUP_CUSTOM_OPTIONS_NAME => [
-                'children' => [
-                    'options' => [
-                        'children' => [
-                            'record' => [
-                                'children' => [
-                                    \Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\CustomOptions::CONTAINER_OPTION => [
-                                        'children' => [
-                                            \Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\CustomOptions::CONTAINER_COMMON_NAME => [
-                                                'children' => [
-                                                    'type' => [
-                                                        'arguments' => [
-                                                            'data' => [
-                                                                'config' => [
-                                                                    'groupsConfig' =>  [
-                                                                        'supplierparameter' => [
-                                                                            'values' => [
-                                                                                'supplierparameter'
+        if (!empty($result[\Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\CustomOptions::GROUP_CUSTOM_OPTIONS_NAME])) {
+
+            return array_merge_recursive($result, [
+                \Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\CustomOptions::GROUP_CUSTOM_OPTIONS_NAME => [
+                    'children' => [
+                        'options' => [
+                            'children' => [
+                                'record' => [
+                                    'children' => [
+                                        \Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\CustomOptions::CONTAINER_OPTION => [
+                                            'children' => [
+                                                \Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\CustomOptions::CONTAINER_COMMON_NAME => [
+                                                    'children' => [
+                                                        'type' => [
+                                                            'arguments' => [
+                                                                'data' => [
+                                                                    'config' => [
+                                                                        'groupsConfig' =>  [
+                                                                            'supplierparameter' => [
+                                                                                'values' => [
+                                                                                    'supplierparameter'
+                                                                                ],
+                                                                                'indexes' => [
+                                                                                    'values'
+                                                                                ]
                                                                             ],
-                                                                            'indexes' => [
-                                                                                'values'
-                                                                            ]
-                                                                        ],
-                                                                        'adjustment' => [
-                                                                            'values' => [
-                                                                                'adjustment'
-                                                                            ],
-                                                                            'indexes' => [
-                                                                                'container_type_static',
-                                                                                'price',
-                                                                                'price_type',
-                                                                                'sku'
+                                                                            'adjustment' => [
+                                                                                'values' => [
+                                                                                    'adjustment'
+                                                                                ],
+                                                                                'indexes' => [
+                                                                                    'container_type_static',
+                                                                                    'price',
+                                                                                    'price_type',
+                                                                                    'sku'
+                                                                                ]
                                                                             ]
                                                                         ]
                                                                     ]
@@ -62,8 +65,11 @@ class CustomOptions
                         ]
                     ]
                 ]
-            ]
-        ]);
+            ]);
+
+        }
+
+        return $result;
 
     }
 
