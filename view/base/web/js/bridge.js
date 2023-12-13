0c2ac1b6a02f7f61ca809e0d42a4cb0eaff6c5eb
Stephan Weikert <weikert@digital-print.net>
2023-12-13 07:59:13 +0100

    Update UUID validation regex
    Modified the UUID validation regex in the `isUuid` function to allow for optional "_standard" suffix at the end of the UUID string.

--- view/base/web/js/bridge.js
+++ view/base/web/js/bridge.js
@@ -334,8 +334,8 @@ define([
 
     function isUuid(uuid, isNullable) {
         return isNullable
-            ? /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(uuid)
-            : /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(uuid);
+            ? /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}(_standard)?$/i.test(uuid)
+            : /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}(_standard)?$/i.test(uuid);
     }
 
     function Bridge(printess, session, config) {
